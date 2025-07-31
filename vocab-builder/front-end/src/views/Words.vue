77c91eebefb3b8ef748455c9db36291b2f9d1efe
XuanNghi24052005 <156789335+XuanNghi24052005@users.noreply.github.com>
2025-07-31 16:10:15 +0700

    Add front-end folder

---
+++ vocab-builder/front-end/src/views/Words.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>Words</h1>
+        <table id ="words" class="ui celled compact table">
+            <thead>
+                <tr>
+                    <th>English</th>
+                    <th>German</th>
+                    <th>French</th>
+                    <th colspan="3"></th>
+                </tr>
+            </thead>
+        <tr v-for="(word, i) in words" :key="i">
+        <td>{{ word.english}}</td>
+        <td>{{ word.german }}</td>
+        <td>{{ word.french }}</td>
+        <td width="75" class="center aligned">
+        <router-link :to="{ name: 'show', params: { id: word._id}}">Show</router-link></td>
+        <td width="75" class="center aligned">
+        <router-link :to="{name: 'edit', params: {id: word._id}}">Edit</router-link></td>
+        <td width="75" class="center aligned" @click.prevent="onDestroy(word._id)">
+      <a :href="`/words/${word._id}`">Destroy</a></td>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import {api} from '../helpers/helpers';
+
+export default {
+    data() {
+        return {
+            words: []
+        };
+    },
+    methods: {
+        async onDestroy(id) {
+            const sure = window.confirm('Are you sure?');
+            if (!sure) return;
+            await api.deleteWord(id);
+            this.flash('Word deleted successfully!', 'success');
+            const newWords = this.words.filter(word => word._id !== id);
+            this.words = newWords;
+        }
+},
+    async mounted() {
+        console.log('Words component mounted, fetching data...');
+        try {
+            this.words = await api.getWords();
+            console.log('Words loaded:', this.words);
+        } catch (error) {
+            console.error('Error loading words:', error);
+        }
+    }
+};
+</script>
